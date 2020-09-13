65e13197fcbc59d2f7b6d92487a93aa319016497
aliraza114 <aliraza3569@gmail.com>
2020-09-13 15:40:21 +0500

    data added

---
+++ src/components/channel/channel.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Flex, Heading, Box } from '@chakra-ui/core'
+
+
+import Item from './item'
+
+
+
+function Channel() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Channel
---
+++ src/components/channel/item.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Heading, Text, Image, Box } from '@chakra-ui/core'
+
+function Item(props) {
+    return (
+        <Box>
+            <Heading >
+                Sell On Eaay
+           </Heading>
+            <Text>
+                Sell to millions of global shoppers on the leading creative marketplace.
+           </Text>
+        </Box>
+    )
+}
+
+export default Item
+
